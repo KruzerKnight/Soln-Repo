a275b1566e084317710ee6aab5047e3bf1c3e663
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-04 22:25:38 +0530

    Update 525-contiguous-array.py

--- 525-contiguous-array/525-contiguous-array.py
+++ 525-contiguous-array/525-contiguous-array.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        d={0:-1}
+        d={0:-1}                        #dict is initialized with -1 cause then only the values from start is also considered like [0,1] and [1,0]
         count=0
         maxi=0
-        for i,n in enumerate(nums,0):
+        for i,n in enumerate(nums,0):   #enumerate here iterates through nums (list) and iterates i (its index)
             if(n==1):
                 count+=1
             else:
@@ -17,3 +17,4 @@ class Solution(object):
             else:
                 d[count]=i
         return maxi
+   # To find the maximum length, we need a dict to store the value of count (as the key) and its associated index (as the value). We only need to save a count value and its index at the first time, when the same count values appear again, we use the new index subtracting the old index to calculate the length of a subarray. A variable maxi is used to to keep track of the current maximum length.
