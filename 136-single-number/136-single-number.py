35739819d809c7cf3650ec0287aa29a2d9eb508d
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-07 15:50:42 +0530

    Update 136-single-number.py

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -14,3 +14,15 @@ class Solution(object):
             if d[i]==1:
                 return i
         #count frequency, return the element with frequency 1
+#faster approach, xor ing a number x with x return 0, thus result will only contain single element
+        
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         res = nums[0]
+#         for i in range(1,len(nums)):
+#             res ^= nums[i]
+#         return res
