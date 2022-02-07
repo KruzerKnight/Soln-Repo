07ba2f6b0a06a4fb3058a0c26141fd9042638397
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-07 15:43:35 +0530

    Time: 2634 ms (10.37%), Space: 14.5 MB (89.52%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxi=max(nums)
+        mini=min(nums)
+        for i in range(mini,maxi):
+            if i not in nums:
+                return i
+        if(mini-1>=0):
+            return mini-1
+        return maxi+1
+        
+        #checkout for edge cases
+        #[1]
+        #[0,1]
