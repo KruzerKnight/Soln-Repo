05f792060d61d795dcfe79f7b11d13e6e172b850
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-07 15:57:05 +0530

    Time: 66 ms (50.51%), Space: 15.6 MB (28.06%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in nums:
+            if d[i]==1:
+                return i
