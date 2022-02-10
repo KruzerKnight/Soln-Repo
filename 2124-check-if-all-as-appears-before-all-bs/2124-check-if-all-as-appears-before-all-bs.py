307961f82be52ce2869e2af84aeda6a9ac87dfa7
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-10 23:17:35 +0530

    Time: 17 ms (78.23%), Space: 13.4 MB (57.48%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -4,11 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l=list(s)
-        l=l[::-1]
-        s1=''.join(l)
-        if(len(set(l))==1):
-            return True
-        if(len(s1)-s1.find('a'))>s.find('b'):
+        if s.rfind('a')>s.find('b') and s.count('a') and s.count('b'):
             return False
         return True
