6588338412f3d69311fe654ad2f39b0b8c6fa8ab
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-11 21:13:07 +0530

    Update 567-permutation-in-string.py

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -18,3 +18,35 @@ class Solution(object):
             if(sorted(s)==sort):
                 return True
         return False
+    #faster method
+#     class Solution(object):
+    
+#     def checkInclusion(self, s1, s2):
+#         if len(s1) > len(s2):
+#             return False
+        
+#         s1mp = [0] * 26
+#         mp = [0] * 26
+
+        
+#         for ch in s1:
+#             s1mp[ord(ch) - ord('a')] += 1
+            
+#         for i in range(len(s1)):
+#             mp[ord(s2[i]) - ord('a')] += 1
+           
+        
+#         i = 0
+#         j = len(s1)
+        
+#         if s1mp == mp:
+#             return True
+        
+#         while j < len(s2):
+#             mp[ord(s2[i]) - ord('a')] -= 1
+#             i += 1
+#             mp[ord(s2[j]) - ord('a')] += 1
+#             j += 1
+#             if mp == s1mp:
+#                 return True
+#         return False
