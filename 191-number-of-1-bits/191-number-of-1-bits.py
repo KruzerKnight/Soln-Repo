23b5bc457310ccf21cd13b53ac0bb68a933e8775
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 15:16:02 +0530

    Update 191-number-of-1-bits.py

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -2,3 +2,22 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         n=str(bin(n)[2:])
         return n.count('1')
+
+#Think of a number in binary n = XXXXXX1000, n - 1 is XXXXXX0111. n & (n - 1) will be XXXXXX0000 which is just remove the last significant 1    
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         count = 0
+#         while(n>0):
+#             n = n & (n-1)
+#             count += 1
+#         return count
+
+#   another faster solution is
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         s = bin(n)
+#         c = 0
+#         for i in range(2,len(s)):
+#             if s[i] == "1":
+#                 c += 1 
+#         return c
