0ae46425d67fe9fe98cf953a948a63e3dc97d117
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 16:30:05 +0530

    Update 344-reverse-string.py

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,7 +1,15 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        mid = len(s) // 2
-        l = len(s) - 1
-        for i in range(mid):
-            s[i], s[l] = s[l], s[i]
-            l -= 1
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i],s[-1-i]=s[-1-i],s[i]
+ # same without negative indexing and faster
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         mid = len(s) // 2
+#         l = len(s) - 1
+#         for i in range(mid):
+#             s[i], s[l] = s[l], s[i]
+#             l -= 1
