985676e92af54ed44b18ea1bf13e0ee1f9d0c17d
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 17:11:41 +0530

    Time: 45 ms (48.10%), Space: 14 MB (62.25%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,5 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        mini=-2**31
+        maxi=2**31-1
         if x<0:
             s=str(x)
             s=s[0]+s[:0:-1]
@@ -7,6 +9,6 @@ class Solution:
             s=str(x)
             s=s[::-1]
         c=int(s)
-        if c<-2**31 or c>2**31-1:
+        if c<mini or c>maxi:
             return 0
         return c
