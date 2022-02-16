653f401a6d696c0345176d5172190fd5c55c3754
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 17:25:21 +0530

    Time: 47 ms (35.10%), Space: 13.9 MB (73.60%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        a=max(x,y)
+        b=min(x,y)
+        a=str(bin(a)[2:])
+        b=str(bin(b)[2:])
+        b='0'*(len(a)-len(b))+b
+        count=0
+        for i in range(len(a)):
+            if(a[i]!=b[i]):
+                count+=1
+        return count
