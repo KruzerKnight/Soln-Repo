15eb59e06e0d51a13ccc88a9bd6d0f588158ac7c
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 17:01:53 +0530

    Update 190-reverse-bits.py

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -7,3 +7,13 @@ class Solution:
             n=n+['0']*(32-len(n))
             n=''.join(n)
         return int(n,2)
+# another method
+# class Solution:
+#     def reverseBits(self, n: int) -> int:
+#         ret, power = 0, 31
+#         while n:
+#             ret += (n & 1) << power
+#             n = n >> 1
+#             power -= 1
+            
+#         return ret
