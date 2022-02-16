5e56e550560b4b3b65d3efb777f8c38585d061bc
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 14:51:12 +0530

    Update 2094-finding-3-digit-even-numbers.py

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -1,3 +1,10 @@
+# We sort the digits list and form the greatest 3 digit number that can be made from the list and traverse from 100 to the number we formerd ( or ) 100 to 999.
+# Each iteration, i is incremented by 2, since only even number is asked
+# We convert the number into a string and store the digits as a ,band c respectively
+# For each even number we first check whether their digits (i.e a ,band c ) are present in the digits list, if not next iteration continues
+# To check if any repeated digits like 222 , 224, 886 etc.., we check a is equal to b or c or b == c, so if digits are repeated we check their count in digits, if the count in digits is equal or greater than that in i we append it to l
+# If not digits are repeated append it to l
+
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         l=[]
@@ -26,3 +33,4 @@ class Solution:
             else:
                 l.append(i)
         return l
+    
