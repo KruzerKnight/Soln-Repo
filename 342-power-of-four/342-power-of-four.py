d212ebff1fd6483a29c169634aab6df5637cc064
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 16:00:27 +0530

    Update 342-power-of-four.py

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -4,3 +4,22 @@ class Solution:
         if(s.count('1'))==1 and n>0:
             if(len(s)%2!=0):
                 return True
+# faster solution
+# class Solution:
+#     def isPowerOfFour(self, n: int) -> bool:
+#         if n == 1:
+#             return True
+#         if n < 1:
+#             return False
+#         return self.isPowerOfFour(n / 4)
+
+# another faster solution
+# class Solution:
+#     def isPowerOfFour(self, n: int) -> bool:
+        
+#         if n <= 0:
+#             return False
+        
+#         power = ceil(math.log(n, 4))
+        
+#         return math.pow(4, power) == n
