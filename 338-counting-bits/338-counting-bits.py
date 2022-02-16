7c9c54e60b727dc31970163cbef082ee55c0a38a
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 17:20:09 +0530

    Update 338-counting-bits.py

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -5,3 +5,19 @@ class Solution:
             s=str(bin(i)[2:])
             l.append(s.count('1'))
         return l
+    
+#learn this
+# class Solution:
+#     def countBits(self, n: int) -> List[int]:
+#         ans = [0] * (n + 1)
+#         for i in range(1, n + 1):
+#             ans[i] = ans[i & (i - 1)] + 1
+#         return ans
+
+# another soln
+# class Solution:
+#     def countBits(self, n: int) -> List[int]:
+#         ans = [0,1]
+#         for j in range(n and int(log2(n))):
+#             ans += [i+1 for i in ans] 
+#         return ans[:n+1]
