440c4a23c64e51fed554dbefb45f8f634cc18450
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-16 14:48:05 +0530

    Update 2133-check-if-every-row-and-column-contains-all-numbers.py

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,3 +1,7 @@
+# first wee are comparing the length of rows with n, cause set(row) will be equal to n, when no elements are repeated
+# then we use enumerate to add index to the element and add all the elements in a list to comapre in columns
+# we sort it w.r.to index and now check the columns
+
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n=len(matrix)
@@ -8,11 +12,11 @@ class Solution:
         for i in matrix:
             ad=[[x,i] for i,x in enumerate(i)]
             l+=ad
-        l=sorted(l, key= lambda x:x[1])
+        l=sorted(l, key= lambda x:x[1])         #sorted w.r.to the column numbers
         for j in range(n):
             count=0
             s=set()
-            for k in range(j*n,(j+1)*n):
+            for k in range(j*n,(j+1)*n):        #iterating through the values from 0->n, n->2n
                 i=l[k]
                 if i[1]==j:
                     s.add(i[0])
