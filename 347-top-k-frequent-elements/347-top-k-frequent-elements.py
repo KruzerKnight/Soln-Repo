e425c20fd5315e4905e9ee666e411e015bcf65a1
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-17 22:28:31 +0530

    Update 347-top-k-frequent-elements.py

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -15,3 +15,18 @@ class Solution:
         for i in range(len(l)-1,len(l)-1-k,-1):
             res.append(l[i][0])
         return res
+    
+    # faster solution
+# from collections import Counter
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+#         # O(1) time 
+#         if k == len(nums):
+#             return nums
+        
+#         # 1. build hash map : character and how often it appears
+#         # O(N) time
+#         count = Counter(nums)   
+
+#         # O(N) time
+#         return [x[0] for x in count.most_common(k)]
