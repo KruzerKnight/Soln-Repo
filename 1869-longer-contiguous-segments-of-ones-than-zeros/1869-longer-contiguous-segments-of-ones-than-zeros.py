8091526111360eafb1a372ce0f7cde88b637787c
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-17 14:12:39 +0530

    Update 1869-longer-contiguous-segments-of-ones-than-zeros.py

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
@@ -25,3 +25,37 @@ class Solution:
         max0=max(max0,count0)
         max1=max(max1,count1)
         return max1>max0
+# instead of calculating at a time:
+# Calculate seperately
+# class Solution:
+#     def checkZeroOnes(self, s: str) -> bool:
+#         one = 0
+#         zero = 0
+        
+#         i = 0
+#         while i < len(s):
+#             cur = 0
+#             while i < len(s) and s[i]=='1':
+#                 cur += 1
+#                 i += 1
+#             one = max(one,cur)
+#             i += 1
+            
+#         i = 0
+#         while i < len(s):
+#             cur = 0
+#             while i < len(s) and s[i]=='0':
+#                 cur += 1
+#                 i += 1
+#             zero = max(zero,cur)
+#             i += 1
+
+
+# Otherwise split it along with 0s and 1s
+# class Solution:
+#     def checkZeroOnes(self, s: str) -> bool:
+#         s1 = s.split('0')
+#         s0 = s.split('1')
+#         r1 = max([len(i) for i in s1])
+#         r0 = max([len(i) for i in s0])
+#         return r1>r0
