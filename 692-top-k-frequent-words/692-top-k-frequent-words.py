df9f6de8c4eeed6c5bf173032019ed8b63fe6043
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-17 21:03:07 +0530

    Time: 95 ms (30.16%), Space: 13.9 MB (88.83%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -15,3 +15,18 @@ class Solution:
         for i in range(k):
             res.append(l[i][0])
         return res
+    
+
+#faster solution
+
+# def topKFrequent(self, words: List[str], k: int) -> List[str]:
+#         dict = {}
+#         for x in words:
+#             if x in dict:
+#                 dict[x] += 1
+#             else:
+#                 dict[x] = 1
+#         res = sorted(dict, key=lambda x: (-dict[x], x))
+#         return res[:k]
+    
+# '-dict[x]' is a sorting key which means sort dictionary based on it's value, here is the number of frequency (negative means ascending order, default is descending order), 'x' is back up sorting key which means if dictionary's value are same then sort it based on the dictionary's key, here is the word alphabetic order (negative means ascending order, default is descending order)
