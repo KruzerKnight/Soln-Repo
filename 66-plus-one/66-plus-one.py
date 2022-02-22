092b4cfc17b518855aac5ef08e1e13d0730d98f2
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-22 22:08:24 +0530

    Update 66-plus-one.py

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -5,3 +5,14 @@ class Solution:
             digits[i]=str(digits[i])
         num=int(''.join(digits))+1
         return list(str(num))
+    
+#     same time for smaller test case but got tle for larger test case
+
+#     class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         num=0
+#         n=len(digits)
+#         for i in range(n):
+#             num+=digits[i]*10**(n-i-1)
+#         num+=1
+#         return list(str(num))
