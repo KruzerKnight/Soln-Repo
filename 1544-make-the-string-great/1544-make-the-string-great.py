c972b7c996d4fbed253bc4a3616bb6b4a3e975f4
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-02-25 21:38:42 +0530

    Update 1544-make-the-string-great.py

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -9,3 +9,14 @@ class Solution:
     
     def makeGood(self, s: str) -> str:
         return cakeGood(s)
+    
+#     fastest solution
+#     class Solution:
+#     def makeGood(self, s: str) -> str:
+#         stack = []
+#         for char in s:
+#             if stack and stack[-1].lower() == char.lower() and stack[-1].isupper() != char.isupper():
+#                 stack.pop()
+#             else:
+#                 stack.append(char)
+#         return "".join(stack)
