f0f8a6696edab25a8055982e27c2f24b5400a9e7
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-07 23:17:56 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.py
@@ -0,0 +1,88 @@
+#User function Template for python3
+'''
+	Your task is to merge the given k sorted
+	linked lists into one list and return
+	the the new formed linked list class.
+
+	Function Arguments:
+	    arr is a list containing the n linkedlist head pointers
+	    n is an integer value
+    
+    node class:
+    
+class Node:
+    def __init__(self,x):
+        self.data = x
+        self.nxt = None
+'''
+class Solution:
+   def mergeKLists(self,arr,K):
+       sortedLinks = []
+       for i in arr:
+           current = i
+           while current:
+               sortedLinks.append(current.data)
+               current = current.next
+       sortedLinks = sorted(sortedLinks)
+       head = Node(sortedLinks[0])
+       current = head
+       for i in sortedLinks[1:]:
+           current.next = Node(i)
+           current = current.next
+       
+       current = head
+       return current
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self,x):
+        self.data=x
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def add(self,x):
+        if self.head is None:
+            self.head=Node(x)
+            self.tail=self.head
+        else:
+            self.tail.next=Node(x)
+            self.tail=self.tail.next
+    
+def printList(head):
+    walk = head
+    while walk:
+        print(walk.data, end=' ')
+        walk=walk.next
+    print()
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n=int(input())
+        line=[int(x) for x in input().strip().split()]
+        
+        heads=[]
+        index=0
+        
+        for i in range(n):
+            size=line[index]
+            index+=1
+            
+            newList = LinkedList()
+            
+            for _ in range(size):
+                newList.add(line[index])
+                index+=1
+            
+            heads.append(newList.head)
+        
+        merged_list = Solution().mergeKLists(heads,n)
+        printList(merged_list)
+
+# } Driver Code Ends
