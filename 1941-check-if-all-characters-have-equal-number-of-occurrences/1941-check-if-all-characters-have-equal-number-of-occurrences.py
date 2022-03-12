dfe5132c7bc9b0d7640005092f9a138f10dfd148
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-13 00:55:30 +0530

    Update 1941-check-if-all-characters-have-equal-number-of-occurrences.py

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -9,3 +9,17 @@ class Solution:
         if len(set(d.values()))==1:
             return True
         return False
+    
+# alternative methods    
+# class Solution:
+# def areOccurrencesEqual(self, s: str) -> bool:
+#     f = s.count(s[0])
+#     for i in set(list(s)):
+#         if s.count(i) != f:
+#             return False
+#     return True
+
+
+# class Solution:
+#     def areOccurrencesEqual(self, s: str) -> bool:
+#         return len(set(Counter(s).values())) == 1
