a78a560c4443bfa8ac21044be18d5328e7023be8
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-13 00:42:53 +0530

    Update 2103-rings-and-rods.py

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -20,3 +20,23 @@ class Solution:
                     count+=1
                     break
         return count
+    
+    
+# from collections import defaultdict
+# class Solution:
+#     def countPoints(self, rings: str) -> int:
+#         d = defaultdict(set)
+#         i = 0
+#         while i < len(rings)-1:
+#             d[rings[i+1]].add(rings[i])
+#             i += 2
+#         res = 0
+#         for key in d:
+#             if len(d[key]) == 3: res += 1
+#         return res
+
+
+# h = defaultdict(set)
+# for i in range(0, len(rings) - 1, 2):
+# 	h[rings[i + 1]].add(rings[i])
+# return sum(len(v) == 3 for v in h.values())
