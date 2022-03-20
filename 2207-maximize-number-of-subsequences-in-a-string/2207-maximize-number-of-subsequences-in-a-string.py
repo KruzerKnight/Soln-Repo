8e71cb69249e0fe2a706f1b5003015a2502f6115
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-20 09:19:22 +0530

    Update 2207-maximize-number-of-subsequences-in-a-string.py

--- 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.py
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.py
@@ -10,3 +10,54 @@ class Solution:
             if c == pattern[0]:
                 cnt += 1
         return ans
+    
+    
+#     Intuition
+# If we add pattern[0], the best option is to add at the begin.
+# If we add pattern[1], the best option is to add at the end.
+
+
+# Explanation
+# Firstly we'll try to find the number of subquence pattern in the current string text
+# Iterate every character c from input string text,
+# cnt1 means the count of character pattern[0],
+# cnt2 means the count of character pattern[1].
+
+# If c == pattern[1],
+# it can build up string pattern with every pattern[0] before it,
+# so we update res += cnt1 as well as cnt2++
+
+# If c == pattern[0],
+# we simply increment cnt1++.
+
+# Then we consider of adding one more character:
+
+# If we add pattern[0],
+# the best option is to add at the begin,
+# res += cnt2
+
+# If we add pattern[1],
+# the best option is to add at the end,
+# res += cnt1
+
+
+# Tip
+# Use two if in my solution,
+# to handle the corner case where pattern[0] == pattern[1].
+
+
+# Complexity
+# Time O(n)
+# Space O(1)
+
+#another solution
+
+# def maximumSubsequenceCount(self, text, pattern):
+#         res = cnt1 = cnt2 = 0
+#         for c in text:
+#             if c == pattern[1]:
+#                 res += cnt1
+#                 cnt2 += 1
+#             if c == pattern[0]:
+#                 cnt1 += 1
+#         return res + max(cnt1, cnt2)
