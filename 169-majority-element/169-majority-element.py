8e51221e5799f3c42c43bc0a6cd8b2ac30738e66
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-22 19:03:30 +0530

    Time: 320 ms (12.21%), Space: 15.5 MB (79.42%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,9 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d={}
+        c=0
+        ele=0
         for i in nums:
-            if i in d:
-                d[i]+=1
+            if c==0:
+                ele=i
+            if i==ele:
+                c+=1
             else:
-                d[i]=1
-        return max(zip(d.values(),d.keys()))[1]
+                c-=1
+        return ele
+     
+        
+        #my first submission, other one is from placement class moore's algorithm
+        # d={}
+        # for i in nums:
+        #     if i in d:
+        #         d[i]+=1
+        #     else:
+        #         d[i]=1
+        # return max(zip(d.values(),d.keys()))[1]
