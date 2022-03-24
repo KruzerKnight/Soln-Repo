1de12254ee8397160904923a1dd3287b6edf012e
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-24 19:15:02 +0530

    Time: 566 ms (59.52%), Space: 21 MB (34.38%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        count=0
+        s=0
+        l=[]
+        for i in people:
+            if i>=limit:
+                count+=1
+            else:
+                l.append(i)
+        i=0
+        j=len(l)-1
+        l.sort()
+        while(i<=j):
+            if (l[i]+l[j])<=limit:
+                i+=1
+            j-=1
+            count+=1
+        return count
