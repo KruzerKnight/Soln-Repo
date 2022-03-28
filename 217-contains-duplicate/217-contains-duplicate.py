6c582e376a427e74554267794c28dc056efa4124
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-03-28 23:09:53 +0530

    Time: 586 ms (52.81%), Space: 26 MB (72.87%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,6 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if(len(nums)!=len(set(nums))):
+            return(True)
+        else:
+            return(False)
