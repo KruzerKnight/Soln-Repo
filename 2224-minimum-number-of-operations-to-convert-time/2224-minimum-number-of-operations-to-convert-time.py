8edbfad2a40d10ecff2878f18b41f84f77ec0660
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-04-03 09:40:49 +0530

    Time: 79 ms (16.67%), Space: 14 MB (66.67%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convertTime(self, current: str, correct: str) -> int:
+        if current==correct:
+            return 0
+        h=int(correct[:2])-int(current[:2])
+        m=int(correct[3:])-int(current[3:])
+        dit=h*60+m
+        count=0
+        print(dit)
+        if dit>=60:
+            count+=dit//60
+            dit=dit%60
+        if dit>=15:
+            count+=dit//15
+            dit=dit%15   
+        if dit>=5:
+            count+=dit//5
+            dit=dit%5    
+        if dit>=1:
+            count+=dit//1
+            dit=dit%1    
+        return count
