2a72dcaa852507f32ed4e8f9cd6358376cec80da
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-04-06 00:15:52 +0530

    Time: 53 ms (47.19%), Space: 14 MB (34.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,28 +1,14 @@
-class Solution(object):
-    def findTheDifference(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        d1={}
-        d2={}
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        d1=defaultdict(int)
+        d2=defaultdict(int)
         for i in s:
-            if i in d1:
-                d1[i]+=1
-            else:
-                d1[i]=1
+            d1[i]+=1
         for i in t:
-            if i in d2:
-                d2[i]+=1
-            else:
-                d2[i]=1
+            d2[i]+=1
+        #print(d1,d2)
         for i in d2:
-            if i in d1:
-                if(d1[i]!=d2[i]):
-                    return i
-            else:
+            if i not in d1:
                 return i
-#create two dictionaries and count the letters in both strings
-#if the second dict has a element more then that is the extra one
-#if not check the count 
+            if d1[i]!=d2[i]:
+                return i
