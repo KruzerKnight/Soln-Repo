01f098745981f681739e07ddadf167e45e902656
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-05-06 23:25:21 +0530

    Time: 138 ms (32.14%), Space: 18.7 MB (5.21%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,20 @@
+#done after ---- https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        k=2
+        l=[['kruz',0]]
+        for i in s:
+            if l[-1][0]==i:
+                l[-1][1]+=1
+                if l[-1][1]==k:
+                    l.pop()
+            else:
+                l.append([i,1])
+        return ''.join(i for i,j in l[1:])
+    
+# Save the character c and its count to the stack.
+# If the next character c is same as the last one, increment the count.
+# Otherwise push a pair (c, 1) into the stack.
+# I used a dummy element ('#', 0) to avoid empty stack.
