dd969bc97994521ec038c9da098fbff26c4854d6
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-05-31 23:25:07 +0530

    Update 1461-check-if-a-string-contains-all-binary-codes-of-size-k.py

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,3 +1,5 @@
 class Solution:
      def hasAllCodes(self, s, k):
         return len({s[i:i + k] for i in range(len(s) - k + 1)}) == 2 ** k
+
+# https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/discuss/2092441/Python-oror-2-Easy-oror-One-liner-with-explanation
