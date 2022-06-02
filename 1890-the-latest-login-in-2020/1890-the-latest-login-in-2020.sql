a2402b22696046af199d71b6037c407e3c1bcdc2
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-06-02 21:03:48 +0530

    Time: 2020 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -2,3 +2,10 @@
 SELECT USER_ID, MAX(TIME_STAMP) AS LAST_STAMP FROM LOGINS
 WHERE (SELECT YEAR(A.TIME_STAMP) FROM LOGINS A WHERE LOGINS.TIME_STAMP=A.TIME_STAMP)='2020'
 GROUP BY USER_ID
+
+# SELECT
+#     user_id,
+#     MAX(time_stamp) AS last_stamp #obtaining latest login for all users
+# FROM Logins
+# WHERE YEAR(time_stamp) = 2020 #filtering for login dates with year 2020 in timestamp
+# GROUP BY user_id;
