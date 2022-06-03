ad897cee06649d981aee1c58c95e607f46d6d88e
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-06-04 00:44:08 +0530

    Time: 730 ms (10.59%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -2,3 +2,6 @@ select activity_date as day, count(distinct user_id) as active_users
 from Activity
 where datediff('2019-07-27', activity_date) <30 AND ACTIVITY_DATE<'2019-08-20'
 group by activity_date
+
+
+#DONT KNOW WHY BUT JUST THE TEST CASES WONT ACCEPT IF THE SECOND CONDITION ISN'T MENTIONED
