51ec594c4f1a7fab47e4be112c50c3e42cc9d411
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-19 12:53:15 +0530

    Time: 2693 ms (62.16%), Space: 103.5 MB (91.38%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,6 +1,7 @@
 class Solution:
     def validPath(self, N: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
+        
         #making an adjacency list from given edges
         al=[]
         for i in range(N):
@@ -10,22 +11,39 @@ class Solution:
             al[n].append(m)
             al[m].append(n)
             
-        #a list for saving distance of the nodes from each other
-        dist=[float('inf')]*N
-        queue=[]  
+#         #a list for saving distance of the nodes from each other
+#         dist=[float('inf')]*N
+#         queue=[]  
         
-        dist[source]=0
+#         dist[source]=0
+#         queue.append(source)
+        
+#         #using shortest path to all nodes from source, thus if a node doesnt have any distance from source then it is not connected thats why using the shortest distance technique
+        
+#         while queue:
+#             node=queue.pop(0)
+            
+#             for i in al[node]:
+#                 if dist[node]+1<dist[i]:
+#                     dist[i]=dist[node]+1
+#                     queue.append(i)
+#         if dist[destination]==float('inf'):
+#             return False
+#         return True
+
+
+#Trying Simple BFS
+
+        queue=[]
+        vis=[0]*N
+        vis[source]=1
         queue.append(source)
-        
-        #using shortest path to all nodes from source, thus if a node doesnt have any distance from source then it is not connected thats why using the shortest distance technique
-        
         while queue:
             node=queue.pop(0)
-            
+            if node==destination:
+                return True
             for i in al[node]:
-                if dist[node]+1<dist[i]:
-                    dist[i]=dist[node]+1
+                if not vis[i]:
                     queue.append(i)
-        if dist[destination]==float('inf'):
-            return False
-        return True
+                    vis[i]=1
+        return False
