0a9c920a3d74e30687f13f1307d4d41a1538585a
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-19 13:05:25 +0530

    Time: 1903 ms (5.03%), Space: 49.9 MB (60.02%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -5,3 +5,8 @@ class Solution:
             d[i[0]]+=1
             d[i[1]]+=1
         return max(zip(d.values(),d.keys()))[1]
+    
+#faster solution:
+
+#         [a] = set(edges[0]) & set(edges[1])
+#         return a
