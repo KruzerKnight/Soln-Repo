3bade1ff2912dbcc1b79ebbce0706b05593e43be
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-19 12:34:09 +0530

    Time: 3220 ms (34.74%), Space: 103.6 MB (86.18%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,5 +1,7 @@
 class Solution:
     def validPath(self, N: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        #making an adjacency list from given edges
         al=[]
         for i in range(N):
             al.append([])
@@ -7,11 +9,15 @@ class Solution:
             n,m=i[0],i[1]
             al[n].append(m)
             al[m].append(n)
+            
+        #a list for saving distance of the nodes from each other
         dist=[float('inf')]*N
         queue=[]  
         
         dist[source]=0
         queue.append(source)
+        
+        #using shortest path to all nodes from source, thus if a node doesnt have any distance from source then it is not connected thats why using the shortest distance technique
         
         while queue:
             node=queue.pop(0)
