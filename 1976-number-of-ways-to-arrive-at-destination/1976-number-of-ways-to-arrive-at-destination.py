86a41886d823a52fc20de592f29eb0929bc1737a
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-20 13:42:54 +0530

    Time: 582 ms (23.27%), Space: 20.8 MB (26.73%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -1,3 +1,5 @@
+#Added a path array to the Dijkstra Algorithm
+
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
         d=defaultdict(list)
@@ -10,7 +12,7 @@ class Solution:
         heapq.heapify(pq)
         
         dist=[float('inf')]*n
-        ways=[0]*n
+        ways=[0]*n # to check/count the ways of minimum traversal 
         
         dist[0]=0
         ways[0]=1
@@ -21,15 +23,20 @@ class Solution:
             dis=node[0]
             prev=node[1]
             
+            if dist[prev]<dis: continue #saves time reduces time complexity
+            
             for i in d[prev]:
                 nxt=i[0]
                 nxtdis=i[1]
                 if dist[nxt]>dis+nxtdis:
                     dist[nxt]=dist[prev]+nxtdis
-                    ways[nxt]=ways[prev]
+                    ways[nxt]=ways[prev] # since this is the minimum path ways for current node is equal to the no of ways of the previous node 
                     heapq.heappush(pq,[dist[nxt],nxt])
                     
-                elif dist[nxt]==dis+nxtdis:
+                elif dist[nxt]==dis+nxtdis: #if both the distance are equal,then you can traverse in both paths, thus adding both of them to no of paths.
                     ways[nxt]=(ways[nxt]+ways[prev])%1000000007
         
         return ways[n-1]
+    
+# We use Dijkstra algorithm to find the Shortest Path from src = 0 to dst = n - 1.
+# While dijkstra, we create additional ways array, where ways[i] keeps the number of shortest path from src = 0 to dst = i. Then the answer is ways[n-1].
