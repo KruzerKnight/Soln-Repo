57c2ebca9c5a4f6541c3246f996109530d43a019
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-21 12:44:26 +0530

    Time: 799 ms (67.37%), Space: 16.1 MB (30.45%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        m=len(grid[0])
+        
+        vis=[[0]*m for _ in range(n)]
+        count=0
+        
+        def dfs(i,j):
+            
+            if i<0 or i>=n or j<0 or j>=m or grid[i][j]==0 or vis[i][j]:
+                return
+            
+            vis[i][j]=1
+            dfs(i-1,j)
+            dfs(i+1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+        
+        for i in range(n):
+            for j in range(m):
+                if (i==0 or j==0 or i==n-1 or j==m-1) and grid[i][j]==1:
+                    dfs(i,j)
+        
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]==1 and not vis[i][j]:
+                    count+=1
+        #print(vis)
+        
+        return count
