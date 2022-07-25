b803b8f7bdca19d40119f2a83c3edcecc55494d3
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-25 20:46:03 +0530

    Time: 1112 ms (32.05%), Space: 15.5 MB (11.78%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,45 +1,24 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # check if source and target are not clear cells
-        if grid[0][0] != 0 or grid[-1][-1] != 0:
-            return -1
         
-        N = len(grid)            
-        # offsets required for all 8 directions
-        offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        n=len(grid)
         
-        q = deque()
-        q.append((0,0)) # starting point
-        visited = {(0, 0)}
+        directions=[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
         
+        if grid[0][0]==0:
+            q=[[1,0,0]]
+        else:
+            q=[]
+        vis=set()
         
-        # finds unvisited clear cells using 8 offsets
-        def get_neighbours(x,y):
-            for x_offset, y_offset in offsets:
-                new_row = x + x_offset
-                new_col = y + y_offset
-                
-                if 0 <= new_row < N and 0 <= new_col < N and not grid[new_row][new_col] and (new_row, new_col) not in visited:
-                    yield (new_row, new_col)                                                
-            
-        
-        current_distance = 1 # start with one clear cell
-        # standard iterative BFS traversal
         while q:
-            length = len(q)
-            
-            # loop through all the cells at the same distance
-            for _ in range(length):
-                row, col = q.popleft()
+            d,x,y=q.pop(0)
+            if [x,y]==[n-1,n-1]:
+                return d
+            for i,j in directions:
+                a,b=x+i,y+j
                 
-                if row == N-1 and col==N-1: # reached target
-                    return current_distance
-                
-                # loop though all valid neignbours
-                for p in get_neighbours(row, col):
-                    visited.add(p)
-                    q.append(p)
-                                    
-            current_distance+=1 # update the level or distance from source
-        
-        return -1                
+                if 0<=a<n and 0<=b<n and (a,b) not in vis and grid[a][b]==0:
+                    vis.add((a,b))
+                    q.append([d+1,a,b])
+        return -1
