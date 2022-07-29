d21a7289e250ac864a3848fd7875184ddc16116a
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-07-30 00:06:35 +0530

    Time: 170 ms (80.43%), Space: 19.7 MB (5.07%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,4 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        nums=[i for i in range(1,n+1)]
-        return list(set(combinations(nums,k)))
+        @cache
+        def getCombos(a: int, b: int) -> List[List[int]]:
+            if b == 1:
+                # base case
+                return [[i] for i in range(a, n + 1)]
+            res = []
+            for i in range(a, n + 1):
+                for combo in getCombos(i + 1, b - 1):
+                    n_combo = [i]
+                    n_combo.extend(combo)
+                    res.append(n_combo)
+            return res
+        return getCombos(1, k)
