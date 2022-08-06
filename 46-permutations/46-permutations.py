9675aade8f476693f72536ab76c56535be5994bf
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-08-06 14:29:01 +0530

    Time: 76 ms (22.99%), Space: 14.2 MB (22.42%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,3 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(set(permutations(nums)))
+        
+        #https://leetcode.com/problems/permutations/discuss/993970/Python-4-Approaches-%3A-Visuals-%2B-Time-Complexity-Analysis
+        
+        def rec(num,perm=[],res=[]):
+            if not num:
+                res.append(perm[::])
+            for i in range(len(num)):
+                l=num[:i]+num[i+1:]
+                perm.append(num[i])
+                rec(l,perm,res)
+                perm.pop()
+            return res
+        return rec(nums)
+
+    
