cc7a2683cd221d47005b439c82ca01910e369bbc
Koushik <76867117+KruzerKnight@users.noreply.github.com>
2022-08-15 18:35:33 +0530

    Time: 105 ms (16.63%), Space: 13.9 MB (80.25%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        res, i = "", 0
+        while num:
+            #print(i,(num//values[i]),(num//values[i]) * numerals[i],num%values[i])
+            res += (num//values[i]) * numerals[i]
+            num %= values[i]
+            i += 1
+        return res
+    
+    #just check whether they can be represented using large numbers, and move through the number
